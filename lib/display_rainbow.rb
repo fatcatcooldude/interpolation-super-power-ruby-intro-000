4abfd913567e409992886e6f8abb99e37a2e5145
Corey Norton <fatcatcooldude@aol.com>
2020-05-05 17:41:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,3 @@ def #display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'vio
   puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo V: violet"
 end
 display_rainbow
-
